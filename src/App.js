b202d56f374c581eecf5720213c88bcb29f0f5c4
Natalie Pulver <npulver00@gmail.com>
2018-11-16 10:43:19 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+          <h1>~~The Friends List~~</h1>
+          <div><input type="ImageUrl"/></div>
+
+          <div><input type="Friends"/></div>
+         
+          <button></button>
+
       </div>
     );
   }
